6079aedf27e3d52867c8f14c2829a4a836c339b7
Evaldas <prevaldas@yahoo.com>
2022-06-03 15:13:53 +0100

    joke category programming

--- src/components/JokeGen.js
+++ src/components/JokeGen.js
@@ -5,25 +5,25 @@ import axios from "axios";
 
 
 
-function JokeGen () {
+function JokeGen() {
 
-            const [joke, setJoke] = useState("");
+    const [joke, setJoke] = useState("");
 
-        const getJoke = () => {
-            axios.get("https://v2.jokeapi.dev/joke//Programming?type=twopar")
+    const getJoke = () => {
+        axios.get("https://v2.jokeapi.dev/joke//Programming?type=twopart")
             .then(res => {
                 console.log(res);
                 setJoke(res.data.setup + " ... " + res.data.delivery);
             }).catch(err => {
             })
-        }
+    }
 
-    return<div className="item2">
-    <center>
-    <h1 className="joke-gen">Joke Generator</h1>
-        <h2 className="joke-space" dangerouslySetInnerHTML={{__html: joke}} />
-    <button className="btn" onClick={getJoke}>Generate joke</button>
-    </center>
+    return <div className="item2">
+        <center>
+            <h1 className="joke-gen">Joke Generator</h1>
+            <h2 className="joke-space" dangerouslySetInnerHTML={{ __html: joke }} />
+            <button className="btn" onClick={getJoke}>Generate joke</button>
+        </center>
     </div>
 }
 export default JokeGen;
